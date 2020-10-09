a230fed22fb9dcdfb16aef2ef758e8c1c08bbadc
Andrés Felipe Carreño <carrenoaf14@gmail.com>
2020-10-09 08:33:53 -0500

    Andrés - Mes

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ app.get("/api/sqlserver/JDEVTAS/getByYM", (req, res) => {
             'VE_VlrOpc, VE_VlrPpto, VE_VlrBnos, VE_VlrSanciones, VE_VlrSeparaciones, ' +
             'VE_VlrMts, VE_QMts, VE_AN8Asesor, VE_NOMAsesor, VE_AN8Cliente,' +
             'VE_NOMCliente, VE_CodCiudad, VE_NOMCiudad, VE_CodRetiro, VE_DesRetiro, VE_VlrDiasPagSepa ' +
-            'FROM JDEVTAS.BICOMERCIAL WHERE VE_Anio >= @VE_Anio1 VE_Mes = @VE_Mes1') //AND 
+            'FROM JDEVTAS.BICOMERCIAL WHERE VE_Anio >= @VE_Anio1 AND VE_Mes = @VE_Mes1') // 
           .then(function (data) {   //${parseInt(VE_Mes)}
             if (data.recordsets[0].length == 0) {
               return res.status(404).json({
