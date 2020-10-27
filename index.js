ba34399201aa08000d0b56f9ed9b08aa099ebd88
felipc1 <carrenoaf14@gmail.com>
2020-10-27 16:17:43 -0500

    Andrés - VE_Estrato

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-'use strict'
+"use strict";
 
 //var fs = require('fs');
 //var https = require('https');
 //var http = require('http');
-const express = require('express');
-const bodyParser = require('body-parser');
-const dbConfig = require('./services/dbconfig.js');
-const webServerConfig = require('./config/web-server.js');
-const mssql = require('mssql');
+const express = require("express");
+const bodyParser = require("body-parser");
+const dbConfig = require("./services/dbconfig.js");
+const webServerConfig = require("./config/web-server.js");
+const mssql = require("mssql");
 
 //import * as fs from "fs";
 //import express from "express";
@@ -30,22 +30,34 @@ let httpsServer;
 //  passphrase: 'marval'
 //}, app);
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
   res.send("Hello there");
 });
 
 app.get("/api/sqlserver/JDEVTAS/getByYM", (req, res) => {
-  
   //var { VE_Anio, //AÑO
   //  VE_Mes //MES
   //} = req.body;
-  var VE_Anio = 2020
-  var VE_Mes = 9
-  if (!VE_Anio ||
-    !VE_Mes) {
+  var VE_Anio = 2020;
+  var VE_Mes = 9;
+  var now = new Date();
+  var year = now.getFullYear();
+  var month = now.getMonth() + 1;
+
+  if (month == 1) {
+    VE_Anio = year - 1;
+    VE_Mes = 12;
+  } else {
+    VE_Anio = year;
+    VE_Mes = month;
+  }
+
+  var VE_Anio = 2020;
+  var VE_Mes = 9;
+  if (!VE_Anio || !VE_Mes) {
     return res.status(412).json({
       ok: false,
-      message: 'Se deben enviar VE_Anio y VE_Mes para la consulta'
+      message: "Se deben enviar VE_Anio y VE_Mes para la consulta",
     });
   }
   try {
@@ -54,7 +66,7 @@ app.get("/api/sqlserver/JDEVTAS/getByYM", (req, res) => {
   } catch (error) {
     return res.status(412).json({
       ok: false,
-      message: 'Se deben enviar el año y mes en formato numérico'
+      message: "Se deben enviar el año y mes en formato numérico",
     });
   }
 
@@ -62,19 +74,19 @@ app.get("/api/sqlserver/JDEVTAS/getByYM", (req, res) => {
 
   async function r1() {
     try {
-
       var info = await get();
       return res.status(200).json({
         ok: true,
-        message: 'Se obtuvieron correctamente los datos',
+        message: "Se obtuvieron correctamente los datos",
         rowsAffected: info.rowsAffected[0],
-        dataList: info.recordsets[0]
+        dataList: info.recordsets[0],
       });
     } catch (error) {
       return res.status(400).json({
         ok: false,
-        message: 'No se ha podido acceder al ID y los demás datos de la consulta',
-        error: error
+        message:
+          "No se ha podido acceder al ID y los demás datos de la consulta",
+        error: error,
       });
     }
   }
@@ -87,53 +99,56 @@ app.get("/api/sqlserver/JDEVTAS/getByYM", (req, res) => {
   //   "VE_VlrMts, VE_QMts, VE_AN8Asesor, VE_NOMAsesor, VE_AN8Cliente, " +
   //   "VE_NOMCliente, VE_CodCiudad, VE_NOMCiudad " +
   // "FROM JDEVTAS.BICOMERCIAL " +
-  // "WHERE VE_Anio = ? " + 
+  // "WHERE VE_Anio = ? " +
   // " AND VE_Mes = ? "
   function get() {
     return new Promise(async function (resolve, reject) {
       let connection = new mssql.ConnectionPool(dbConfig);
-      connection.connect().then(function () {
-        new mssql.Request(connection)
-          .input('VE_Anio1', mssql.Int, VE_Anio)
-          .input('VE_Mes1', mssql.Int, VE_Mes)
-          .query('SELECT VE_Ukid, VE_Anio, VE_Mes, VE_Tipo, VE_Oper, VE_Sucursal, VE_NomSucursal, ' +
-            'VE_CodProyecto, VE_NomProyecto, VE_UndDisponible, VE_FecVtaRet, VE_TipoPry,' +
-            'VE_Inmueble, VE_UndsVtas, VE_UndsRet, VE_UndsOpc, VE_VlrVtas, VE_VlrRet,' +
-            'VE_VlrOpc, VE_VlrPpto, VE_VlrBnos, VE_VlrSanciones, VE_VlrSeparaciones, ' +
-            'VE_VlrMts, VE_QMts, VE_AN8Asesor, VE_NOMAsesor, VE_AN8Cliente,' +
-            'VE_NOMCliente, VE_CodCiudad, VE_NOMCiudad, VE_CodRetiro, VE_DesRetiro, VE_VlrDiasPagSepa ' +
-            'FROM JDEVTAS.BICOMERCIAL WHERE VE_Anio >= @VE_Anio1 AND VE_Mes = @VE_Mes1') // 
-          .then(function (data) {   //${parseInt(VE_Mes)}
-            if (data.recordsets[0].length == 0) {
-              return res.status(404).json({
-                ok: true,
-                message: 'No se obtuvieron resultados',
-                dataList: data.recordsets[0],
-                rowsAffected: data.rowsAffected
+      connection
+        .connect()
+        .then(function () {
+          new mssql.Request(connection)
+            .input("VE_Anio1", mssql.Int, VE_Anio)
+            .input("VE_Mes1", mssql.Int, VE_Mes)
+            .query(
+              "SELECT VE_Ukid, VE_Anio, VE_Mes, VE_Tipo, VE_Oper, VE_Sucursal, VE_NomSucursal, " +
+                "VE_CodProyecto, VE_NomProyecto, VE_UndDisponible, VE_FecVtaRet, VE_TipoPry," +
+                "VE_Inmueble, VE_UndsVtas, VE_UndsRet, VE_UndsOpc, VE_VlrVtas, VE_VlrRet," +
+                "VE_VlrOpc, VE_VlrPpto, VE_VlrBnos, VE_VlrSanciones, VE_VlrSeparaciones, " +
+                "VE_VlrMts, VE_QMts, VE_AN8Asesor, VE_NOMAsesor, VE_AN8Cliente," +
+                "VE_NOMCliente, VE_CodCiudad, VE_NOMCiudad, VE_CodRetiro, VE_DesRetiro, VE_VlrDiasPagSepa, VE_Estrato " +
+                "FROM JDEVTAS.BICOMERCIAL WHERE VE_Anio >= @VE_Anio1 AND VE_Mes = @VE_Mes1"
+            ) //
+            .then(function (data) {
+              //${parseInt(VE_Mes)}
+              if (data.recordsets[0].length == 0) {
+                return res.status(404).json({
+                  ok: true,
+                  message: "No se obtuvieron resultados",
+                  dataList: data.recordsets[0],
+                  rowsAffected: data.rowsAffected,
+                });
+              } else {
+                resolve(data);
+              }
+              connection.close();
+            })
+            .catch(function (error) {
+              console.log(error);
+              return res.status(400).json({
+                ok: false,
+                message: "No se ha podido realizar la consulta con la BD",
+                error: error,
               });
-            } else {
-              resolve(data);
-            }
-            connection.close();
-          })
-          .catch(function (error) {
-            console.log(error);
-            return res.status(400).json({
-              ok: false,
-              message: 'No se ha podido realizar la consulta con la BD',
-              error: error
             });
-          });
-      })
+        })
         .catch(function (error) {
           console.log(error);
           return res.status(400).json({
             ok: false,
-            message: 'No se ha podido conectar con la BD 22'
+            message: "No se ha podido conectar con la BD 22",
           });
         });
-
-
 
       // try {
       //   connection = mssql.connect(dbConfig)
@@ -163,11 +178,9 @@ app.get("/api/sqlserver/JDEVTAS/getByYM", (req, res) => {
       // }
     });
   }
-
 });
 
-app.post('/api/sqlserver/JDEVTAS/getPlano', (req, res) => {
-
+app.post("/api/sqlserver/JDEVTAS/getPlano", (req, res) => {
   //var CodPry = req.headers.CodPry;
   var CodPry = req.body.CodPry;
   //if (!CodPry) {
@@ -189,19 +202,19 @@ app.post('/api/sqlserver/JDEVTAS/getPlano', (req, res) => {
 
   async function r1() {
     try {
-
       var info = await get();
       return res.status(200).json({
         ok: true,
-        message: 'Se obtuvieron correctamente los datos',
+        message: "Se obtuvieron correctamente los datos",
         rowsAffected: info.rowsAffected[0],
-        dataList: info.recordsets[0]
+        dataList: info.recordsets[0],
       });
     } catch (error) {
       return res.status(400).json({
         ok: false,
-        message: 'No se ha podido acceder al ID y los demás datos de la consulta',
-        error: error
+        message:
+          "No se ha podido acceder al ID y los demás datos de la consulta",
+        error: error,
       });
     }
   }
@@ -209,40 +222,43 @@ app.post('/api/sqlserver/JDEVTAS/getPlano', (req, res) => {
   function get() {
     return new Promise(async function (resolve, reject) {
       let connection = new mssql.ConnectionPool(dbConfig);
-      connection.connect().then(function () {
-        new mssql.Request(connection)
-          .input('CodPry1', mssql.NChar, CodPry)
-          .query('SELECT CodLot, EtqEst, EtqPrc, EtqAre, EtqVlm, EtqFev, EtqVlb, EtqNcl, EtqClr FROM JDEVTAS.PLANOBI WHERE CodPry = @CodPry1')
-          .then(function (data) {
-            if (data.recordsets[0].length == 0) {
-              return res.status(404).json({
-                ok: true,
-                message: 'No se obtuvieron resultados',
-                dataList: data.recordsets[0],
-                rowsAffected: data.rowsAffected
+      connection
+        .connect()
+        .then(function () {
+          new mssql.Request(connection)
+            .input("CodPry1", mssql.NChar, CodPry)
+            .query(
+              "SELECT CodLot, EtqEst, EtqPrc, EtqAre, EtqVlm, EtqFev, EtqVlb, EtqNcl, EtqClr FROM JDEVTAS.PLANOBI WHERE CodPry = @CodPry1"
+            )
+            .then(function (data) {
+              if (data.recordsets[0].length == 0) {
+                return res.status(404).json({
+                  ok: true,
+                  message: "No se obtuvieron resultados",
+                  dataList: data.recordsets[0],
+                  rowsAffected: data.rowsAffected,
+                });
+              } else {
+                resolve(data);
+              }
+              connection.close();
+            })
+            .catch(function (error) {
+              console.log(error);
+              return res.status(400).json({
+                ok: false,
+                message: "No se ha podido realizar la consulta con la BD",
+                error: error,
               });
-            } else {
-              resolve(data);
-            }
-            connection.close();
-          })
-          .catch(function (error) {
-            console.log(error);
-            return res.status(400).json({
-              ok: false,
-              message: 'No se ha podido realizar la consulta con la BD',
-              error: error
             });
-          });
-      })
+        })
         .catch(function (error) {
           console.log(error);
           return res.status(400).json({
             ok: false,
-            message: 'No se ha podido conectar con la BD 22'
+            message: "No se ha podido conectar con la BD 22",
           });
         });
-
     });
   }
 });
