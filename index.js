081822de09b617a463324b47ffcd94b90bc3149b
Ferrucho20 <58154131+Ferrucho20@users.noreply.github.com>
2021-08-02 15:49:59 -0500

    cambios planos

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //}, app);
 
 app.get("/", (req, res) => {
-  res.send("tttttt");
+  res.send("api marvall s.a");
 });
 
 app.get("/api/sqlserver/JDEVTAS/month", (req, res) => {
