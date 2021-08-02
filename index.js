f0e6554704ec9abe9d696371f1dfa5a048800a63
Ferrucho20 <58154131+Ferrucho20@users.noreply.github.com>
2021-08-02 14:29:29 -0500

    sss

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //}, app);
 
 app.get("/", (req, res) => {
-  res.send("marval marval marval");
+  res.send("llllllllll");
 });
 
 app.get("/api/sqlserver/JDEVTAS/month", (req, res) => {
